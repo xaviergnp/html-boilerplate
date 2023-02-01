495b0fcaa5071a45bb3c7a2e6bd5dab15cd13e1f
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-02 00:08:40 +0800

    Finish fundamentals part 1 assignment

--- js/fundamentals-part-1/script.js
+++ js/fundamentals-part-1/script.js
@@ -12,7 +12,7 @@
 let admin, userName;
 userName = "John";
 admin = userName;
-alert(admin);
+// alert(admin);
 
 // #2 Giving the right name
 let ourPlanetName = "Earth";
@@ -25,3 +25,21 @@ function someCode() {
 
 const BIRTHDAY = '18.04.1982';
 const age = someCode(BIRTHDAY);
+
+// Assignment
+console.log(23+97);
+
+console.log(1+2+3+4+5+6);
+
+console.log((4 + 6 + 9)/77);
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
