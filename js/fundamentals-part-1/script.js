07d9425e5d9e2f095c786823dd7145082ee99f85
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-01 19:32:17 +0800

    Do working with variables

--- js/fundamentals-part-1/script.js
+++ js/fundamentals-part-1/script.js
@@ -1,7 +1,12 @@
-console.log("Hi User!")
+// console.log("Hi User!")
 // let user='John', age=25, message='Hello';
 // message = 'Hello';
-let user = 'Jake';
-let age = 18;
-let message = 'Hi!';
-alert(message);
+// let user = 'Jake';
+// let age = 18;
+// let message = 'Hi!';
+// alert(message);
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
