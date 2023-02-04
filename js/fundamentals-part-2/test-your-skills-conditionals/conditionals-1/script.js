e7087a9da4f0fe6d484f2473cc4109ffe5788f62
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-04 14:19:52 +0800

    Copy js exercise

---
+++ js/fundamentals-part-2/test-your-skills-conditionals/conditionals-1/script.js
@@ -0,0 +1,12 @@
+let season = "summer";
+let response;
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector(".preview");
+section.innerHTML = " ";
+let para1 = document.createElement("p");
+para1.textContent = response;
+section.appendChild(para1);
