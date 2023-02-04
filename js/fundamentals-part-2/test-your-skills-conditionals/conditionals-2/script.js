5e0b811e1c1f8f6d7805a4116f36988e8434f795
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-04 22:05:58 +0800

    Copy exercise code

---
+++ js/fundamentals-part-2/test-your-skills-conditionals/conditionals-2/script.js
@@ -0,0 +1,18 @@
+let response;
+let score = 75;
+let machineActive = false;
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector("section");
+
+let para1 = document.createElement("p");
+let para2 = document.createElement("p");
+
+para1.textContent = "Your socre is ${ score }";
+para2.textCntent = response;
+
+section.appendChild(para1);
+section.appendChild(para2);
