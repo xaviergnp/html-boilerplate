ba72d2b46b777a15acb299337c0563e401910916
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-04 14:59:24 +0800

    Add selection dropdown for Seasons

--- js/fundamentals-part-2/test-your-skills-conditionals/conditionals-1/script.js
+++ js/fundamentals-part-2/test-your-skills-conditionals/conditionals-1/script.js
@@ -1,20 +1,22 @@
-let season = "summer";
+let season = document.querySelector("select");
 let response;
-
 // Add your code here
-
-if (season === "summer") {
-    response = "The current season is Summer!";
-} else if (season === "winter") {
-    response = "The current season is Winter!";
-} else {
-    response = "The current season is UNDEFINED!!!"
+season.addEventListener("change", setSeason);
+function setSeason() {
+    if (season.value === "summer") {
+        response = "The current season is Summer!";
+    } else if (season.value === "winter") {
+        response = "The current season is Winter!";
+    } else {
+        response = "The current season is UNDEFINED!!!"
+    }
+    const section = document.querySelector(".preview");
+    section.innerHTML = " ";
+    let para1 = document.createElement("p");
+    para1.textContent = response;
+    section.appendChild(para1);
 }
+
 
 // Don't edit the code below here!
 
-const section = document.querySelector(".preview");
-section.innerHTML = " ";
-let para1 = document.createElement("p");
-para1.textContent = response;
-section.appendChild(para1);
