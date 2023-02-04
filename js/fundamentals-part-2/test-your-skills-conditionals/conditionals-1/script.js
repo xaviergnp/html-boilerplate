2a243998959c71d970caf6436969d2479d4ae43f
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-04 14:33:43 +0800

    Add logical statements to check season

--- js/fundamentals-part-2/test-your-skills-conditionals/conditionals-1/script.js
+++ js/fundamentals-part-2/test-your-skills-conditionals/conditionals-1/script.js
@@ -3,6 +3,14 @@ let response;
 
 // Add your code here
 
+if (season === "summer") {
+    response = "The current season is Summer!";
+} else if (season === "winter") {
+    response = "The current season is Winter!";
+} else {
+    response = "The current season is UNDEFINED!!!"
+}
+
 // Don't edit the code below here!
 
 const section = document.querySelector(".preview");
