e9c1800f56892ac57c87f80a5bb70aac92db3b72
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-05 12:37:01 +0800

    Copy exercise code for styles and script

---
+++ js/fundamentals-part-2/test-your-skills-conditionals/conditionals-4/script.js
@@ -0,0 +1,21 @@
+let machineActive=true;
+let pwd = "cheese";
+
+let machineResult;
+let pwdResult;
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector("section");
+
+
+let para1 = document.createElement("p");
+let para2 = document.createElement("p");
+
+para1.textContent = machineResult;
+para2.textContent = pwdResult;
+
+section.appendChild(para1);
+section.appendChild(para2);)
