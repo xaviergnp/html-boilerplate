d007aa000f8f5abc6fbcf1430221a334f51340c3
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-06 17:00:48 +0800

    Copy exercise

---
+++ js/fundamentals-part-2/exercises/exercise-2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let number = Number(prompt("Enter a number"));
+
+function numberChecker() {
+    if(number === 6){
+        return true;
+    } else {
+        return false;
+    }
+}
