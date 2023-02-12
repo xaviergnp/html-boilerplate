9ad14493dd59e4fc084e99be71c7330727c12134
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 17:17:18 +0800

    Change let to const

--- js/fundamentals-part-4/launch-countdown/script.js
+++ js/fundamentals-part-4/launch-countdown/script.js
@@ -1,8 +1,8 @@
 "use strict";
-let output = document.querySelector(".output");
+const output = document.querySelector(".output");
 output.innerHTML = "";
 for(let x=10; x>=0; x--) {
-    let para = document.createElement("p");
+    const para = document.createElement("p");
     para.textContent = x==10? "Countdown 10" : x==0? "Blast Off!" : x;
     output.appendChild(para);
 }
