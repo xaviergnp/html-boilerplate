980407fe1d23f87124c49402c0b6c58e8e45afa8
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 18:30:46 +0800

    Add filter to guests

--- js/fundamentals-part-4/launch-countdown/guest-list/script.js
+++ js/fundamentals-part-4/launch-countdown/guest-list/script.js
@@ -7,17 +7,21 @@ function filterGuest(guests) {
     } else {
         admittedGuest+=guests;
     }
-    return [refusedGuest,admittedGuest];
+    if (guests == "Bruce") console.log(refusedGuest,admittedGuest);
+    return ([refusedGuest,admittedGuest]);
 }
 
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
-
+filterGuest();
+console.log(filterGuest());
+console.log("1"+people.forEach(filterGuest));
 const admitted = document.querySelector('.admitted');
 const refused = document.querySelector('.refused');
 admitted.textContent = 'Admit: ';
 refused.textContent = 'Refuse: ';
 
 // loop starts here
-let filteredGuest = people.forEach(filterGuest());
+let filteredGuest = people.forEach(filterGuest);
+console.log(filterGuest);
 refused.textContent += filterGuest[0];
 admitted.textContent += filterGuest[1];
