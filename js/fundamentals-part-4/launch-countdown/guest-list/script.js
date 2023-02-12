21d177e959b84fadeffd53c73c11c62e27d814f2
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 18:41:34 +0800

    Change filter array to loop

--- js/fundamentals-part-4/launch-countdown/guest-list/script.js
+++ js/fundamentals-part-4/launch-countdown/guest-list/script.js
@@ -1,27 +1,20 @@
 "use strict";
-function filterGuest(guests) {
-    let refusedGuest = "";
-    let admittedGuest = "";
-    if (guests == "Phil" || guests == "Lola") {
-        refusedGuest+=guests;
-    } else {
-        admittedGuest+=guests;
-    }
-    if (guests == "Bruce") console.log(refusedGuest,admittedGuest);
-    return ([refusedGuest,admittedGuest]);
-}
 
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
-filterGuest();
-console.log(filterGuest());
-console.log("1"+people.forEach(filterGuest));
+
 const admitted = document.querySelector('.admitted');
 const refused = document.querySelector('.refused');
 admitted.textContent = 'Admit: ';
 refused.textContent = 'Refuse: ';
 
 // loop starts here
-let filteredGuest = people.forEach(filterGuest);
-console.log(filterGuest);
-refused.textContent += filterGuest[0];
-admitted.textContent += filterGuest[1];
+for (let guest of people) {
+    if(guest == "Phil" || guest == "Lola") {
+        refused.textContent += `${guest}, `;
+    } else {
+        admitted.textContent += `${guest}, `;
+    }
+    console.log(guest);
+}
+admitted.textContent = admitted.textContent.slice(-2);
+refused.textContent = refused.textContent.slice(-2)
