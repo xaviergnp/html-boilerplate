50a09ba4f928a9ad56ac34aec5b0ab29836b1039
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 17:24:35 +0800

    Copy script initial code

--- js/fundamentals-part-4/launch-countdown/guest-list/script.js
+++ js/fundamentals-part-4/launch-countdown/guest-list/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+// loop starts here
+
+// refused.textContent += ;
+// admitted.textContent += ;
