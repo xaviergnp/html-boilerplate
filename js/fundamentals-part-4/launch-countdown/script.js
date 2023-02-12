14610ced686b423d89e6563153c77cef83304cc7
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 17:11:32 +0800

    Copy initial script code

--- js/fundamentals-part-4/launch-countdown/script.js
+++ js/fundamentals-part-4/launch-countdown/script.js
@@ -1,1 +1,8 @@
 "use strict";
+let output = document.querySelector(".output");
+output.innerHTML = "";
+for(let x=10; x>=0; x--) {
+    let para = document.createElement("p");
+    para.textContent = "";
+    output.appendChild(para);
+}
