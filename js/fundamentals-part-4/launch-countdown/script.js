dda263cd37e7cc11f7bf464b88d07c243e35481a
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-12 17:15:10 +0800

    Create loop and condition for countdown

--- js/fundamentals-part-4/launch-countdown/script.js
+++ js/fundamentals-part-4/launch-countdown/script.js
@@ -3,6 +3,6 @@ let output = document.querySelector(".output");
 output.innerHTML = "";
 for(let x=10; x>=0; x--) {
     let para = document.createElement("p");
-    para.textContent = "";
+    para.textContent = x==10? "Countdown 10" : x==0? "Blast Off!" : x;
     output.appendChild(para);
 }
