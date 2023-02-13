4620321eb9e3a730791a8f13bf159ac8b00fd34a
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 16:57:54 +0800

    Add style to event target

--- js/dom-manipulation/follow-along-lesson/script.js
+++ js/dom-manipulation/follow-along-lesson/script.js
@@ -1,3 +1,7 @@
 "use strict";
 const buttonPopup = document.querySelector("#btn");
 buttonPopup.addEventListener("click", () => alert("Hello World!"));
+buttonPopup.addEventListener("click", function(e){
+    console.log(e.target);
+    e.target.style.background = "blue";
+});
