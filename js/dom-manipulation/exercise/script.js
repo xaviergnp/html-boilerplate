4814cd712816ab9459a3b84d1307bb2f1427226e
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 16:27:22 +0800

    Copy exercise code

--- js/dom-manipulation/exercise/script.js
+++ js/dom-manipulation/exercise/script.js
@@ -1,1 +1,8 @@
 "use strict";
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
