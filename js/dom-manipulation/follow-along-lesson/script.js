3031f9c7c724883d636e56de0dd8a2d1af2219c5
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-24 10:54:05 +0800

    Create iterator for all buttons

--- js/dom-manipulation/follow-along-lesson/script.js
+++ js/dom-manipulation/follow-along-lesson/script.js
@@ -1,5 +1,13 @@
 "use strict";
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function (e) {
-    console.log(e);
+    console.log(e.target.style.background="blue");
+});
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", ()=> {
+        alert(button.id);
+    });
 });
