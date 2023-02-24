8e8b04054f9b7daddc143c564519669bb9fa3061
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-24 10:33:47 +0800

    Print click event

--- js/dom-manipulation/follow-along-lesson/script.js
+++ js/dom-manipulation/follow-along-lesson/script.js
@@ -1,7 +1,5 @@
 "use strict";
-const buttonPopup = document.querySelector("#btn");
-buttonPopup.addEventListener("click", () => alert("Hello World!"));
-buttonPopup.addEventListener("click", function(e){
-    console.log(e.target);
-    e.target.style.background = "blue";
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function (e) {
+    console.log(e);
 });
