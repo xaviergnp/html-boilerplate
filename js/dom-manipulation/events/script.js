992fbb45603d0f2deab2c7bcc28e6ffe48109722
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-24 10:27:37 +0800

    Change onlick to event listener

--- js/dom-manipulation/events/script.js
+++ js/dom-manipulation/events/script.js
@@ -1,3 +1,10 @@
 "use strict";
+// Method 2
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World!");
+
+// Method 3
 const btn = document.querySelector("#btn");
-btn.onclick = () => alert("Hello World!");
+btn.addEventListener("click", () => {
+    alert("Hello World!!!");
+});
