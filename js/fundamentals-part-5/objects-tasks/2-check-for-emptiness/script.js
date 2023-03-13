5907e440ca77e07d36560f822c189636ab22534b
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-03-13 11:49:22 +0800

    Create function to check if object is empty

--- js/fundamentals-part-5/objects-tasks/2-check-for-emptiness/script.js
+++ js/fundamentals-part-5/objects-tasks/2-check-for-emptiness/script.js
@@ -1,1 +1,10 @@
 "use strict";
+function isEmpty(obj) {
+    let empty = true;
+    for(let key in obj) empty = false;
+    return empty;
+}
+
+let schedule = {};
+schedule["8:30"] = "get up";
+alert(isEmpty(schedule));
